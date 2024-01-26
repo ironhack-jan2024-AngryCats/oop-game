66db736456fd55fee6b42732dcf4786a2c40417d
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-01-26 13:00:19 +0100

    implement functionality to display and move one obstacle

--- js/main.js
+++ js/main.js
@@ -34,9 +34,52 @@ class Player {
 }
 
 
+
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 100;
+        this.width = 20;
+        this.height = 20;
+        this.domElm = null;
+
+        this.createDomElement();
+    }
+    createDomElement() {
+        // step1: create the element
+        this.domElm = document.createElement("div");
+
+        // step2: add content or modify
+        this.domElm.setAttribute("class", "obstacle");
+        this.domElm.style.width = this.width + "vw"
+        this.domElm.style.height = this.height + "vh"
+        this.domElm.style.left = this.positionX + "vw";
+        this.domElm.style.bottom = this.positionY + "vh";
+
+        //step3: append to the dom: `parentElm.appendChild()`
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElm);
+    }
+    moveDown(){
+        this.positionY = this.positionY - 10;
+        this.domElm.style.bottom = this.positionY + "vh"; 
+    }
+}
+
+
+
+
 const player = new Player();
 
+const newObstacle = new Obstacle();
 
+setInterval(() => {
+    newObstacle.moveDown();
+}, 500);
+
+
+
+// add event listeners
 document.addEventListener("keydown", (e) => {
     if (e.code === 'ArrowLeft') {
         player.moveLeft();
