b263187553706f15085cb496d00ad2e47ea3efb5
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-01-26 12:34:10 +0100

    implement functionality to display and move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
-console.log("hello world");
+class Player {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 0;
+        this.width = 20;
+        this.height = 20;
+        this.domElm = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // step1: create the element
+        this.domElm = document.createElement("div");
+
+        // step2: add content or modify
+        this.domElm.setAttribute("id", "player");
+        this.domElm.style.width = this.width + "vw"
+        this.domElm.style.height = this.height + "vh"
+        this.domElm.style.left = this.positionX + "vw";
+        this.domElm.style.bottom = this.positionY + "vh";
+        
+        //step3: append to the dom: `parentElm.appendChild()`
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElm);
+    }
+    moveLeft(){
+        this.positionX--;
+        this.domElm.style.left = this.positionX + "vw";
+    }
+    moveRight(){
+        this.positionX++;
+        this.domElm.style.left = this.positionX + "vw";
+    }
+}
+
+
+const player = new Player();
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === 'ArrowLeft') {
+        player.moveLeft();
+    } else if (e.code === 'ArrowRight') {
+        player.moveRight();
+    }
+});
+
